62fbe8c50e445c5b00412515cacb8ab84d8ab45e
CaoBo <13629101894@139.com>
2022-01-19 21:53:21 +0800

    导入SpringBoot核心jar包，并测试

---
+++ src/main/java/com/wenjie/album/AlbumApplication.java
@@ -0,0 +1,15 @@
+package com.wenjie.album;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AlbumApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(AlbumApplication.class, args);
+        }
+
+}
